97a0e738a4ea8478aa04eb8150ddf4c7baba7e2a
ashutosh <201851029@iiitvadodara.ac.in>
2018-12-02 17:45:42 +0530

    Updated files

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+echo "Giving executable permissions ..."
 sudo chmod 755 db-sh-txt
 sudo chmod 755 nameof
-echo "Giving executable permissions ..."
+sudo chmod 755 uninstall.sh
 echo "Creating Directories ..."
 sudo mkdir /usr/share/nameof
 sudo cp -r . /usr/share/nameof
 sudo cp /usr/share/nameof/nameof /usr/local/bin
 echo "Successfully copied contents ..."
 echo "Successfully Installed database."
-echo "run \"nameof <9-digit-ID>\"" 
+echo "run \"nameof <9-digit-ID>\" or \"nameof <search-keyword>\"" 
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd /usr/share/
+sudo rm -r nameof
+echo "Deleted all data"
+cd /usr/local/bin/
+sudo rm nameof
+echo "Deleted command files from bin"
